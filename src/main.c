c0b4b4a483fa47f821da80cb92f0ac8e4cda8d0f
Ben Oztalay <boztalay@gmail.com>
2025-10-25 19:54:04 -0700

    COBS encode USB responses

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "stm32f0xx.h"
 #include "stm32f0xx_hal.h"
 
+#include "cobs.h"
 #include "usb_device.h"
 #include "usb_cdc.h"
 #include "can.h"
@@ -30,6 +31,7 @@ int main(void)
     CAN_RxHeaderTypeDef rx_msg_header;
     uint8_t rx_msg_data[8] = {0};
     uint8_t msg_buf[OKCAN_MAX_MESSAGE_LENGTH];
+    uint8_t cobs_buf[OKCAN_MAX_MESSAGE_LENGTH * 2];
 
     while(1) {
         usb_cdc_process();
@@ -44,7 +46,8 @@ int main(void)
 
                 // Transmit message via USB-CDC
                 if (msg_len) {
-                    usb_cdc_sendData(msg_buf, msg_len);
+                    uint8_t cobs_len = cobs_encode(msg_buf, msg_len, cobs_buf);
+                    usb_cdc_sendData(cobs_buf, cobs_len);
                 }
             }
         }
